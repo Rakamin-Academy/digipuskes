b77fb87a1b35775c5e853264635bbf13cda55d37
Robeet <sukocorobertus@gmail.com>
2020-11-29 12:47:52 +0700

    adding jadwal dokter and rekam medis pasien

---
+++ components/RekamMedis.vue
@@ -0,0 +1,303 @@
+<template>
+  <div class="main-container">
+        <h1 class="header">Rekam Medis Pasien</h1>
+        <div class="biodata">
+            <div class="photo-container"></div>
+            <div class="biodata-container">
+                <div class="biodata-content left ">
+                <div class="biodata-item">
+                    <h5>Nama</h5>
+                    <p>Nama Disini</p>
+                </div>
+                <div class="biodata-item">
+                    <h5>Tanggal Lahir</h5>
+                    <p>07/06/2018</p>
+                </div>
+                <div class="biodata-item">
+                    <h5>Email orang tua/wali</h5>
+                    <p>john.doe@example.com</p>
+                </div>
+                </div>
+                <div class="biodata-content right">
+                    <div class="biodata-item">
+                        <h5>Nomor Telpon</h5>
+                        <p>0878122345</p>
+                    </div>
+                    <div class="biodata-item">
+                        <h5>Email</h5>
+                        <p>john.doe@example.com</p>
+                    </div>
+                    </div>
+            </div>
+        </div>
+        <div class="table-outer-container">
+            <h3 class="table-title">Daftar Tindakan</h3>
+            <div class="table-header-container">
+                <h4 class="table-header col1">Jenis Tindakan</h4>
+                <h4 class="table-header col2">Tanggal</h4>
+                <h4 class="table-header col3">Waktu</h4>
+                <h4 class="table-header col4">Dokter</h4>
+                <h4 class="table-header col5">Vaksin</h4>
+            </div>
+            <ul class="content-list isi-rekam">
+                <li class="content-item">
+                    <p class="list-item nama col1">Imunisasi BCG</p>
+                    <p class="list-item spesialisasi col2">10/10/2020</p>
+                    <p class="list-item jadwal-hari col3">09.00</p>
+                    <p class="list-item waktu col4">Dr. James</p>
+                    <p class="list-item waktu col5">BCS</p>
+                    <div class="content-update">
+                        <!-- Insert Icons -->
+                        <i class="fas fa-pencil-alt edit-btn"></i>
+                        <i class="far fa-check-square check-btn"></i>
+                    </div>
+                </li>
+                <li class="content-item">
+                    <p class="list-item nama col1">Imunisasi BCG</p>
+                    <p class="list-item spesialisasi col2">10/10/2020</p>
+                    <p class="list-item jadwal-hari col3">09.00</p>
+                    <p class="list-item waktu col4">Dr. James</p>
+                    <p class="list-item waktu col5">BCS</p>
+                    <div class="content-update">
+                        <!-- Insert Icons -->
+                        <i class="fas fa-pencil-alt edit-btn"></i>
+                        <i class="far fa-check-square check-btn"></i>
+                    </div>
+                </li>
+                <li class="content-item">
+                    <p class="list-item nama col1">Imunisasi BCG</p>
+                    <p class="list-item spesialisasi col2">10/10/2020</p>
+                    <p class="list-item jadwal-hari col3">09.00</p>
+                    <p class="list-item waktu col4">Dr. James</p>
+                    <p class="list-item waktu col5">BCS</p>
+                    <div class="content-update">
+                        <!-- Insert Icons -->
+                        <i class="fas fa-pencil-alt edit-btn"></i>
+                        <i class="far fa-check-square check-btn"></i>
+                    </div>
+                </li>
+                <li class="content-item">
+                    <p class="list-item nama col1">Imunisasi BCG</p>
+                    <p class="list-item spesialisasi col2">10/10/2020</p>
+                    <p class="list-item jadwal-hari col3">09.00</p>
+                    <p class="list-item waktu col4">Dr. James</p>
+                    <p class="list-item waktu col5">BCS</p>
+                    <div class="content-update">
+                        <!-- Insert Icons -->
+                        <i class="fas fa-pencil-alt edit-btn"></i>
+                        <i class="far fa-check-square check-btn"></i>
+                    </div>
+                </li>
+                <li class="content-item">
+                    <p class="list-item nama col1">Imunisasi BCG</p>
+                    <p class="list-item spesialisasi col2">10/10/2020</p>
+                    <p class="list-item jadwal-hari col3">09.00</p>
+                    <p class="list-item waktu col4">Dr. James</p>
+                    <p class="list-item waktu col5">BCS</p>
+                    <div class="content-update">
+                        <!-- Insert Icons -->
+                        <i class="fas fa-pencil-alt edit-btn"></i>
+                        <i class="far fa-check-square check-btn"></i>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="rencana-tindakan">
+            <h3 class="div-header">Rencana Tindakan</h3>
+            <div class="tindakan-wrapper">
+                <div class="tindakan-content">
+                    <div class="tindakan-item">
+                        <h5>Nama tindakan</h5>
+                        <input type="text">
+                    </div>
+                    <div class="tindakan-item">
+                        <h5>Tanggal</h5>
+                        <input type="date" name="" id="">
+                    </div>
+                    <div class="tindakan-item">
+                        <h5>Vaksin</h5>
+                        <input type="text">
+                    </div>
+                    </div>
+                <div class="tindakan-keterangan">
+                    <h5>Keterangan</h5>
+                    <textarea placeholder="beri keterangan..." rows="4" cols="50"></textarea>
+                </div>
+                
+            </div>
+            <button class="input-btn">INPUT</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    * {
+    font-family: 'Lato', sans-serif;
+}
+html {
+    /* width: 1440px; */
+    /* height: 1200px; */
+    background-color: #F6F8FB;
+}
+body {
+    margin: 0;
+}
+.main-container {
+    margin-left: 256px;
+    margin-top: 88px;
+}
+ul {
+    list-style: none;
+    padding-left: 0;
+    margin: 0;
+}
+h1, h2, h3, h4, h5 {
+    margin: 0;
+}
+li, p {
+    margin: 0;
+}
+h1 {
+    font-size: 40px;
+    padding-left: 52px;
+    margin-bottom: 15px;
+    margin-top: 35px;
+}
+/* TABLE BASE STYLINGS */
+.table-outer-container {
+    width: 1110px;
+    margin-right: 45px;
+    margin-left: 29px;
+    background-color: white;
+    border-radius: 8px;
+    box-shadow: 0px 12px 26px rgba(16, 30, 115, 0.06);
+}
+.table-header-container {
+    /* width: 100%; */
+    display: flex;
+    flex-direction: row;
+    background-color:rgba(232, 232, 232, 0.2);
+}
+.table-title {
+   padding-left: 24px; 
+   padding-top: 24px;
+   padding-bottom: 18px;
+}
+.table-header {
+    padding-left: 24px;
+    padding-top: 19px;
+    padding-bottom: 17px;
+}
+.col1, .col2, .col3, .col4, .col5 {
+    width: 160px;
+}
+.content-item {
+    display: flex;
+    flex-direction: row;
+    border-top: 1px solid #E8E8E8;
+    border-bottom: 1px solid #E8E8E8;
+}
+.content-item p {
+    padding-left: 24px;
+    padding-top: 18px;
+    padding-bottom: 18px;
+}
+/* CUSTOM STYLE */
+/* REKAM MEDIS */
+.biodata {
+    display: flex;
+    display: none;
+    flex-direction: row;
+    max-height: 134px;
+    max-width: 968px;
+    margin-left: 29px;
+    justify-content: flex-start;
+}
+.photo-container {
+    height: 134px;
+    width: 134px;
+    background-color: black;
+    border-radius: 50%;
+}
+.biodata-container {
+    display: flex;
+    flex-direction: row;
+    width: 816px;
+    margin-left: 18px;
+    justify-content: space-between;
+}
+.biodata-item p, .biodata-item h5{
+    font-size: 15px;
+}
+.biodata-content {
+    align-items: center;
+}
+.biodata-item, .tindakan-item {
+    display: flex;
+    flex-direction: row;
+    height: 19px;
+    margin: auto;
+    margin-bottom: 19px;
+}
+.biodata-item h5, .tindakan-item h5 {
+    width: 165px;
+}
+.isi-rekam {
+    max-height: 280px;
+    overflow: scroll;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+}
+.isi-rekam::-webkit-scrollbar {
+    display: none;
+}
+.rencana-tindakan {
+    margin-top: 92px;
+    margin-left: 29px;
+    height: 271px;
+}
+.rencana-tindakan h3 {
+    margin-bottom: 39px;
+}
+.tindakan-wrapper {
+    display: flex;
+    flex-direction: row;
+}
+.tindakan-keterangan {
+    margin-left: 74px;
+}
+.tindakan-keterangan input {
+    width: 479px;
+    height: 100px;
+    padding: 5px;
+}
+.input-btn {
+    height: 45px;
+    width: 201px;
+    background-color: rgba(47,128,237,1);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.3);
+    color: white;
+    border: none;
+    outline: none;
+    border-radius: 4px;
+}
+.content-update {
+    align-items: center;
+    margin-top: auto;
+    margin-bottom: auto;
+}
+.edit-btn {
+    font-size: 20px;
+    margin-right: 20px;
+}
+.check-btn {
+    font-size: 30px;
+    color: #2958E6;
+}
+</style>>
